7c7c9bcc9c7523f3502e8e88138793dfc62237e4
lanserdi <lanserdi@163.com>
2019-08-19 21:58:39 +0800

    update

--- public/check.js
+++ public/check.js
@@ -1,1 +1,135 @@
-!function(e,n){"object"==typeof exports&&"object"==typeof module?module.exports=n():"function"==typeof define&&define.amd?define([],n):"object"==typeof exports?exports.goma=n():e.goma=n()}(window,function(){return function(e){var n={};function t(r){if(n[r])return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}return t.m=e,t.c=n,t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{enumerable:!0,get:r})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,n){if(1&n&&(e=t(e)),8&n)return e;if(4&n&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(t.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&n&&"string"!=typeof e)for(var o in e)t.d(r,o,function(n){return e[n]}.bind(null,o));return r},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="",t(t.s=2)}([function(e,n,t){"use strict";t.r(n),t.d(n,"ajax",function(){return r}),t.d(n,"getImageBySrc",function(){return o}),t.d(n,"readFile",function(){return i}),t.d(n,"readLabelFile",function(){return u}),t.d(n,"readSourceFile",function(){return a}),t.d(n,"readdir",function(){return c});let r=(e,n,t)=>{let r=new XMLHttpRequest;r.responseType="json",r.onreadystatechange=function(){200==r.status&&4==r.readyState&&t&&t(r)},n?r.open("POST",e,!0):r.open("GET",e,!0),r.send(n)},o=(e,n)=>{var t=new Image;t.src=e,t.onload=function(){n&&n(this)}},i=(e,n,t)=>{r("/api/readfile/"+(e=e||"")+"/"+(n=n||""),null,function(e){t&&t(e.response)})},u=(e,n)=>{i("label",e,function(e){n&&n(e)})},a=(e,n)=>{i("source",e,function(e){n&&n(e)})},c=(e,n)=>{"source"==e||"label"==e?r("/api/readdir/"+e,null,function(e){n&&n(e.response)}):n&&n(null)}},,function(e,n,t){"use strict";t.r(n);t(3);var r=t(0);const o={canContinue:!0,currentDownloadIndex:0,files:null,sectionWidth:200};var i;i=function(){document.querySelector("#app").style.width=`${o.files.length*o.sectionWidth}px`,function e(){r.default.readLabelFile(o.files[o.currentDownloadIndex],function(n){if(!n.data)return;const t=JSON.parse(n.data);!function(e){const n=document.createElement("section");n.style.width=`${o.sectionWidth}px`;let t=`<h2>${e.imagePath}</h2>`,r=e.shapes.map(function(e,n){return`<div class="label-content">\n\t\t\t<img src="${e.imageData}"/>\n\t\t\t<div class="label">${e.label}</div>\n\t\t</div>`}).join("");t+=`<div class="labels">${r}</div>`,n.innerHTML=t,document.querySelector("#app").append(n)}(t),o.currentDownloadIndex++,o.currentDownloadIndex>=o.files.length?o.canContinue=!1:e()})}()},r.default.readdir("label",function(e){o.files=e.data||[],i&&i()})},function(e,n,t){}])});
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["goma"] = factory();
+	else
+		root["goma"] = factory();
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/check/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/check/index.js":
+/*!****************************!*\
+  !*** ./src/check/index.js ***!
+  \****************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.scss */ \"./src/check/index.scss\");\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../utils */ \"./src/utils.js\");\n\n\n\n\nconst model = {\n  canContinue: true,\n  currentDownloadIndex: 0,\n  files: null,\n\n  sectionWidth: 200,\n}\n\n\n\ngetAllLabelFiles(function(){\n  document.querySelector('#app').style.width = `${model.files.length * model.sectionWidth}px`;\n  renderSinglePaperModel()\n})\n\n\nfunction getAllLabelFiles(cb){\n  _utils__WEBPACK_IMPORTED_MODULE_1__[\"default\"].readdir('label', function(res){\n    model.files = res.data || []\n    cb && cb()\n  })\n}\n\nfunction renderSinglePaperModel(){\n  _utils__WEBPACK_IMPORTED_MODULE_1__[\"default\"].ajax(`/api/readfile/label/${model.files[model.currentDownloadIndex]}`, null, function(client){\n    let res = client.response;\n    if(!res.data) return\n    const paperModel = JSON.parse(res.data)\n    generatePaper(paperModel)\n    model.currentDownloadIndex++\n\n    if(model.currentDownloadIndex >= model.files.length){\n      model.canContinue = false\n    }else{\n      renderSinglePaperModel()\n    }\n  })\n}\n\n\nfunction generatePaper(paperModel){\n  const sectionEl = document.createElement('section')\n\n  sectionEl.style.width = `${model.sectionWidth}px`\n\n  let html = `<h2>${paperModel.imagePath}</h2>`\n\n  let imgs = paperModel.shapes.map(function(shape, index){\n    return `<div class=\"label-content\">\n      <img src=\"${shape.imageData}\"/>\n      <div class=\"label\">${shape.label}</div>\n    </div>`\n  }).join('')\n\n  html += `<div class=\"labels\">${imgs}</div>`\n\n  sectionEl.innerHTML = html\n  document.querySelector('#app').append(sectionEl)\n}\n\n//# sourceURL=webpack://goma/./src/check/index.js?");
+
+/***/ }),
+
+/***/ "./src/check/index.scss":
+/*!******************************!*\
+  !*** ./src/check/index.scss ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack://goma/./src/check/index.scss?");
+
+/***/ }),
+
+/***/ "./src/utils.js":
+/*!**********************!*\
+  !*** ./src/utils.js ***!
+  \**********************/
+/*! exports provided: ajax, getImageBySrc */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"ajax\", function() { return ajax; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"getImageBySrc\", function() { return getImageBySrc; });\nlet ajax = (url, data, cb) => {\n  let client = new XMLHttpRequest();\n  client.responseType = \"json\";\n  client.onreadystatechange = () => {\n    if(client.status == 200 && client.readyState == 4) cb && cb(client);\n  }\n  data ? client.open(\"POST\", url, true) : client.open(\"GET\", url, true);\n  client.send(data);\n};\n\nlet getImageBySrc = (src, cb) => {\n  let img = new Image();\n  img.src = src;\n  img.onload = () => {\n    cb && cb(img);\n  }\n};\n\n\n//# sourceURL=webpack://goma/./src/utils.js?");
+
+/***/ })
+
+/******/ });
+});
